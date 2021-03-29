4a64a4be4513e5acb48281efebbe47cc2a8def08
Phil Haines <phil.haines92@gmail.com>
2021-03-29 11:22:33 -0600

    Initial for marc

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+	<header>
+		<h1>Task Tracker</h1>
+		<Button text="Add Tast" color="green" />
+	</header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+	name: "Header",
+	props: {
+		title: {
+			type: String,
+		},
+	},
+	components: {
+		Button,
+	},
+};
+</script>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 3rem;
+}
+</style>
